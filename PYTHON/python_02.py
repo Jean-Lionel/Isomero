1bcc08a86b597ccfa011ae155d9b77c16adab169
Jean Lionel <nijeanlionel@gmail.com>
2022-01-06 21:13:58 +0200

    les variables

---
+++ PYTHON/python_02.py
@@ -0,0 +1,12 @@
+## variable x
+
+igiharuro_ca_1 = int(input("Andika igiharuro ca 1 : "))
+igiharuro_ca_2 = int(input("Andika igiharuro ca 2 : "))
+
+## GUTERANYA 
+print(f"{igiharuro_ca_1} + {igiharuro_ca_2} = { igiharuro_ca_1 + igiharuro_ca_2}")
+print(f"{igiharuro_ca_1} - {igiharuro_ca_2} = { igiharuro_ca_1 - igiharuro_ca_2}")
+print(f"{igiharuro_ca_1} / {igiharuro_ca_2} = { igiharuro_ca_1 / igiharuro_ca_2}")
+print(f"{igiharuro_ca_1} x {igiharuro_ca_2} = { igiharuro_ca_1 * igiharuro_ca_2}")
+print(f"{igiharuro_ca_1} MODULO {igiharuro_ca_2} = { igiharuro_ca_1 % igiharuro_ca_2}")
+
