89e9b53761e1acc37c617e620107c5ab2062ae5b
Jean Lionel <nijeanlionel@gmail.com>
2022-01-06 05:47:50 +0200

    first step in python

---
+++ C/Hello.c
@@ -0,0 +1,8 @@
+#include "stdio.h"
+// LANGAGE C
+
+int main(int argc, char const *argv[])
+{
+	printf("UMWAKA MWIZA 2022");
+	return 0;
+}
---
+++ PYTHON/python_01.py
@@ -0,0 +1,13 @@
+## FUNCTION PRINT
+"""
+print ni function idufasha kugira affichage y'ikintu
+"""
+def andika(ijambo):
+	print(ijambo)
+
+## guteranya
+igiharuro_ca_1 = 10
+igiharuro_ca_2 = 10
+inyishu = igiharuro_ca_1 + igiharuro_ca_2
+
+print(f"INYISHU NI {inyishu}")
