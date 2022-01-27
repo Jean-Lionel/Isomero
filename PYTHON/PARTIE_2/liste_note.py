417074738073d44b177eb4dc9722454bd2bcb845
Jean Lionel <nijeanlionel@gmail.com>
2022-01-27 20:52:41 +0200

    plus sur les liste

---
+++ PYTHON/PARTIE_2/liste_note.py
@@ -0,0 +1,24 @@
+
+11.1 Méthodes associées aux listes
+
+    11.1.1 .append()
+    11.1.2 .insert()
+    11.1.3 del
+    11.1.4 .remove()
+    11.1.5 .sort()
+    11.1.6 sorted()
+    11.1.7 .reverse()
+    11.1.8 .count()
+    11.1.9 Particularités des méthodes associées aux listes
+
+11.2 Construction d'une liste par itération
+11.3 Test d'appartenance
+11.4 Copie de listes
+11.5 Liste de compréhension
+
+    11.5.1 Nombres pairs compris entre 0 et 30
+    11.5.2 Jeu sur la casse des mots d'une phrase
+    11.5.3 Formatage d'une séquence avec 60 caractères par ligne
+    11.5.4 Formatage FASTA d'une séquence (avec la ligne de commentaire)
+    11.5.5 Sélection des carbones alpha dans un fichier pdb
+
