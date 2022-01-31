bbab11e3635ad7ce3c32f9b774de9e20c2af8c1f
Jean Lionel <nijeanlionel@gmail.com>
2022-01-31 05:21:17 +0200

    correction des fautes d'orthographe

--- PYTHON/PARTIE_2/revision.py
+++ PYTHON/PARTIE_2/revision.py
@@ -10,11 +10,9 @@ with open("list_pays.txt", 'r', encoding = "utf8") as file:
 
 for i in range(5):
 	q = random.randint(0,len(liste_pays)-1)
-	print(f"reponse est {liste_pays[q][1]}")
 	response = input(f"Quel est la capital du {liste_pays[q][0]} ? : ")
 
-
 	if  response.lower().strip() == liste_pays[q][1].lower().strip():
-		print("Filicitation vous avez reussi")
+		print("Félicitation vous avez réussi")
 	else:
-		print(f"Echeque c'est {liste_pays[q][1]}")
+		print(f"Echec c'est {liste_pays[q][1]}")
