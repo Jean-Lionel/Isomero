5ff330395d38b10f4cc2a8910c923ed2883782e9
Jean Lionel <nijeanlionel@gmail.com>
2022-02-03 21:06:39 +0200

    calcul des poitnt

--- PYTHON/PARTIE_2/revision.py
+++ PYTHON/PARTIE_2/revision.py
@@ -7,6 +7,7 @@ with open("list_pays.txt", 'r', encoding = "utf8") as file:
 	for line in file.readlines():
 		liste_pays.append(line.split("#"))
 
+total_obtenu = 0
 
 for i in range(5):
 	q = random.randint(0,len(liste_pays)-1)
@@ -14,5 +15,9 @@ for i in range(5):
 
 	if  response.lower().strip() == liste_pays[q][1].lower().strip():
 		print("Félicitation vous avez réussi")
+		total_obtenu += 1
 	else:
 		print(f"Echec c'est {liste_pays[q][1]}")
+
+
+print(f"Le resultat obtenu est de {total_obtenu} /5")
